97a19947d22bdd2569a8ac49917481157a02035d
projectyelen <projectyelen@tuta.io>
2022-06-10 15:13:59 +0200

    commy

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import * as React from 'react'
+import { graphql } from "gatsby";
 import Layout from '../components/layout'
 import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle='Home Page'>
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <p>I'm making this by following the Gatsby Tutorial. Ajmo mi cijevi vidit</p>
       <StaticImage
         alt='Image description'
         src='../images/E1oMV3QVgAIr1NT.jpg'
